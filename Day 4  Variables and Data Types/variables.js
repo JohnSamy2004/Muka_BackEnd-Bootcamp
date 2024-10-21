af56f2d932b222cdcfce97c246839264572b6b46
JohnSamy2004 <jsamy2950@gmail.com>
2024-10-21 22:10:09 +0300

    Variables

---
+++ Day 4  Variables and Data Types/variables.js
@@ -0,0 +1,16 @@
+// Using var
+var x = 10;
+console.log("var x:", x); // var allows redeclaration
+x = 20;
+console.log("Updated var x:", x);
+
+// Using let
+let y = 30;
+console.log("let y:", y); // let does not allow redeclaration
+y = 40;
+console.log("Updated let y:", y);
+
+// Using const
+const z = 50;
+console.log("const z:", z); // const does not allow reassignment or redeclaration
+// z = 60; // Uncommenting this line will throw an error
