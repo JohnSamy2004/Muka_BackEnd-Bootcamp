2d80355856555c28a41e0fb488d247a770a2bafa
JohnSamy2004 <jsamy2950@gmail.com>
2024-10-21 22:11:35 +0300

    Type_Coercion

---
+++ Day 4  Variables and Data Types/typeCoercion.js
@@ -0,0 +1,7 @@
+console.log('5' + 2); // String concatenation: "52"
+console.log('5' - 2); // Numeric subtraction: 3
+console.log(true + 1); // Boolean to number: 2
+console.log(false + 10); // Boolean to number: 10
+console.log(5 * '4'); // String to number: 20
+console.log(5 == '5'); // Type coercion during comparison: true
+console.log(5 === '5'); // Strict comparison: false (no type coercion)
