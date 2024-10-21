3831f4cee9edc0de61120c685c5462a811e6578c
JohnSamy2004 <jsamy2950@gmail.com>
2024-10-21 22:12:47 +0300

    Operators

---
+++ Day 4  Variables and Data Types/operators.js
@@ -0,0 +1,6 @@
+console.log(5 + 3);  // Addition: 8
+console.log(5 - 3);  // Subtraction: 2
+console.log(5 * 3);  // Multiplication: 15
+console.log(5 / 3);  // Division: 1.666...
+console.log(5 % 3);  // Modulus: 2
+console.log(5 ** 3); // Exponentiation: 125
