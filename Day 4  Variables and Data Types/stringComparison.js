93bcd897671e765da90c90db4e6800e101bedca4
JohnSamy2004 <jsamy2950@gmail.com>
2024-10-21 22:21:51 +0300

    String_Comparison

---
+++ Day 4  Variables and Data Types/stringComparison.js
@@ -0,0 +1,9 @@
+const str1 = "hello";
+const str2 = "hello";
+
+// This will equal true and will print "The strings are equal."
+if (str1 === str2) {
+  console.log("The strings are equal.");
+} else {
+  console.log("The strings are not equal.");
+}
