adf000862a5f22832509ddf9a2e3d9c775b04430
JohnSamy2004 <jsamy2950@gmail.com>
2024-10-24 21:16:03 +0300

    push & pop

---
+++ Day 7 Arrays/array2.js
@@ -0,0 +1,11 @@
+const arr = [1, 2, 3, 4, 5];
+
+let add = arr.push(2); //add a value '2' to after last index in array and return the length
+
+console.log(add);
+console.log(arr);
+
+let del = arr.pop(); //delete a value '2' from last index in array and return the deleted value
+
+console.log(del);
+console.log(arr);
