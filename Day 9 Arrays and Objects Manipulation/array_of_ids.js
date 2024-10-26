3d36a872b911343b09c162a5a8f14481f061ab65
JohnSamy2004 <jsamy2950@gmail.com>
2024-10-26 22:59:22 +0300

    array of ids

---
+++ Day 9 Arrays and Objects Manipulation/array_of_ids.js
@@ -0,0 +1,23 @@
+function getListStudents() {
+    return [
+        {
+            id: 1,
+            firstname: 'Ziad',
+            location: 'San Francisco'
+        },
+        {
+            id: 2,
+            firstname: 'James',
+            location: 'Columbia'
+        },
+        {
+            id: 5,
+            firstname: 'Serena',
+            location: 'San Francisco'
+        }
+    ];
+}
+
+function getListStudentsIds(students){
+    return students.map(student => student.id);
+}
