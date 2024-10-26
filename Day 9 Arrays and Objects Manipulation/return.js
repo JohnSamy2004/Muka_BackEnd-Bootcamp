773ec3c2cea1ddf539e03bceca802d0cff37542f
JohnSamy2004 <jsamy2950@gmail.com>
2024-10-26 22:52:36 +0300

    return array of objects

---
+++ Day 9 Arrays and Objects Manipulation/return.js
@@ -0,0 +1,21 @@
+function getListStudents() {
+    return [
+        {
+            id: 1,
+            firstname: 'Ziad',
+            location: 'San Francisco'
+        },
+        {
+            id: 2,
+            firstname: 'James',
+            location: 'Columbia'
+        },
+        {
+            id: 5,
+            firstname: 'Serena',
+            location: 'San Francisco'
+        }
+    ];
+}
+
+console.log(getListStudents());
