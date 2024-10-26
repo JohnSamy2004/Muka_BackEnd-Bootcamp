249f3a8678fe3bfdd6049e359372c7e69fb459c7
JohnSamy2004 <jsamy2950@gmail.com>
2024-10-26 10:59:38 +0300

    array of objects

---
+++ Day 8 Objects/arr_of_objs.js
@@ -0,0 +1,29 @@
+const Book = [
+    {
+        title: 'The Great Gatsby',
+        author: 'F.Scott Fitzgerald',
+        yearPublished: 1925
+    },
+    {
+        title: 'ex1',
+        author: 'Ahmed',
+        yearPublished: 1996
+    },
+    {
+        title: 'ex2',
+        author: 'Sameh',
+        yearPublished: 1926
+    },
+    {
+        title: 'ex3',
+        author: 'Amin',
+        yearPublished: 1930
+    },
+    {
+        title: 'ex4',
+        author: 'Youssef',
+        yearPublished: 1966
+    }
+];
+
+console.log(Book);
